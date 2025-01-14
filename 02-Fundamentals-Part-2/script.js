4cfd508734dc3368b00b58b8920dbe2b17f6bf78
stoyj <stoyanka.kamenova@gmail.com>
2025-01-14 11:46:27 +0200

    console.log(fruitProcessor(5, 8));

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -12,3 +12,52 @@
 // const interface = 'Audio';
 // const privete = 534;
 
+
+// Functions
+
+function logger() {
+	console.log('My name is Annie');
+}
+
+// calling / running / invoking function
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+	const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+	return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 8));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+
+
+// Function Declarations vs. Expressions
+
+// Function declaration
+function calcAge1(birthYeah) {
+	return 2037 - birthYeah;
+}
+const age1 = calcAge1(1991);
+
+// Function expression
+const calcAge2 = function (birthYeah) {
+	return 2037 - birthYeah;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+
+// Arrow functions
+
+
+
