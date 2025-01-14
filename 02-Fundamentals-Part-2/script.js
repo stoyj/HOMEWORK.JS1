3ba79c9fc66a25768aa94108e87abe15aad67f09
stoyj <stoyanka.kamenova@gmail.com>
2025-01-14 13:25:46 +0200

    Arrow functions Functions Calling Other Functions Reviewing Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -60,6 +60,55 @@ console.log(age1, age2);
 
 
 // Arrow functions
+// const calcAge3 = birthYeah => 2037 - birthYeah;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+// const yearsUntilRetirement = (birthYeah, firstName) => {
+// 	const age = 2037 - birthYeah;
+// 	const retirement = 65 - age;
+// 	// return retirement;
+// 	return `${firstName} retires in ${retirement} years`;
+// }
+// console.log(yearsUntilRetirement(1991, 'Annie')); console.log(yearsUntilRetirement(1980, 'Bob'));
+
+
+// Functions Calling Other Functions
+function cutFruitPieces(fruit) {
+	return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+	const applePieces = cutFruitPieces(apples);
+	const orangesPieces = cutFruitPieces(oranges);
+
+	const juice = `Juice with ${applePieces} piece of apple and ${orangesPieces} pieces of orange.`;
+	return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+
+// Reviewing Functions
+const calcAge = function (birthYeah) {
+	return 2037 - birthYeah;
+}
+
+const yearsUntilRetirement = function (birthYeah, firstName) {
+	const age = calcAge(birthYeah);
+	const retirement = 65 - age;
+
+	if (retirement > 0) {
+		console.log(`${firstName} retires in ${retirement} years`);
+	} else {
+		console.log(`${firstName} has already retired 🎉`);
+		return - 1;
+	}
+}
+console.log(yearsUntilRetirement(1991, 'Annie'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+
+
+
+// Introduction to Arrays
 
 
 
