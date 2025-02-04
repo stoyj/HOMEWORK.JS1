88ad592ddbc99b25ab2f95e044ff9630a425d848
stoyj <stoyanka.kamenova@gmail.com>
2025-02-04 17:45:18 +0200

    correction

--- 13-Advanced DOM and Events [PROJECT]/script.js
+++ 13-Advanced DOM and Events [PROJECT]/script.js
@@ -3,7 +3,7 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelector('.btn--show-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const nav = document.querySelector('.nav');
@@ -25,7 +25,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(btn => btn.addEventListenr('click', openModal));
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -44,27 +44,28 @@ btnScrollTo.addEventListener('click', function (e) {
 
   console.log(e.target.getBoundingClientRect());
 
-  console.log('Current scroll (X/Y', window.pageXOffset, window.pageYOffset);
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
 
   console.log(
     'height/width viewport',
     document.documentElement.clientHeight,
     document.documentElement.clientWidth
   );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
-
-// Scrolling
-// window.scrollTo(
-//   s1coords.left + window.pageXOffset,
-//   s1coords.top + window.pageYOffset
-// );
-
-// window.scrollTo({
-//   left: s1coords.left + window.pageXOffset,
-//   top: s1coords.top + window.pageYOffset,
-//   behavior: 'smooth',
-// });
-section1.scrollIntoView({ behavior: 'smooth' });
 
 ///////////////////////////////////////
 // Page navigation
@@ -84,7 +85,7 @@ section1.scrollIntoView({ behavior: 'smooth' });
 document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
 
-  // Matching strategy -- Съвпадаща стратегия
+  // Matching strategy
   if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
@@ -92,27 +93,28 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 });
 
 ///////////////////////////////////////
-// Tabbed component -- Компонент с раздели
+// Tabbed component
 
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
 
-  // Guard clause  -- Защитна клауза
+  // Guard clause
   if (!clicked) return;
 
-  // Remove active classes -- Премахване на активни класове
+  // Remove active classes
   tabs.forEach(t => t.classList.remove('operations__tab--active'));
   tabsContent.forEach(c => c.classList.remove('operations__content--active'));
 
-  // Activate tab -- Активиране на раздела
+  // Activate tab
   clicked.classList.add('operations__tab--active');
 
-  // Activate content area -- Активиране на областта със съдържание
+  // Activate content area
   document
-    .querySelector(`.operations__content--${clicked.dateset.tab}`)
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
 
+///////////////////////////////////////
 // Menu fade animation
 const handleHover = function (e) {
   if (e.target.classList.contains('nav__link')) {
