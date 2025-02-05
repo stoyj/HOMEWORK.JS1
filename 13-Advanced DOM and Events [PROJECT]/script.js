057271841687143a173ecd76946438c4aa99dc62
stoyj <stoyanka.kamenova@gmail.com>
2025-02-05 15:29:50 +0200

    Comment- important

--- 13-Advanced DOM and Events [PROJECT]/script.js
+++ 13-Advanced DOM and Events [PROJECT]/script.js
@@ -80,7 +80,7 @@ btnScrollTo.addEventListener('click', function (e) {
 // });
 
 // 1. Add event listener to common parent element
-// 2. Determine what element originated the event
+// 2. Determine what element originated the event - Много важно
 
 document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
